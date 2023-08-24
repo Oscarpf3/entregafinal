a5b685d7adbda496bb53007d1f62dca2c4fd40d5
Oscarpf3 <opferrando@gmail.com>
2023-08-23 23:31:41 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+
+//import './App.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom"; 
+
+import Header from "./componentes/layout/Header";
+import Nav from "./componentes/layout/Nav";
+import Footer from "./componentes/layout/Footer";
+
+import ContactoPage from './pages/ContactoPage';
+import HomePage from './pages/HomePage';
+import NosotrosPage from './pages/NosotrosPage';
+import ServiciosPage from './pages/ServiciosPage';
+import NovedadesPage from "./pages/NovedadesPage";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="nosotros" element={<NosotrosPage />} />
+          <Route path="servicios" element={<ServiciosPage />} />
+          <Route path="contacto" element={<ContactoPage />} />
+          <Route path="novedades" element={<NovedadesPage />} />
+        </Routes>
+
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/componentes/layout/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../styles/components/layout/Header.css' ;
+const Header = (props) => { return (
+<header>
+    <div classname="holder">
+        <img src="img/logo3.png"  width="350" alt="Graciela Lacour" />
+    </div>
+</header>
+); }
+
+
+
+
+export default Header;
